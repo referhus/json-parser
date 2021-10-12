f8c7ccbf905969285e34f6204dac523fbc75ca9e
Winterh <kotfimn@mail.ru>
2021-10-13 03:37:28 +0700

    Перебор элементов файла и создание форм (немного работает)

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ window.addEventListener("DOMContentLoaded", () => {
        inputFile = document.querySelector('.file'),
        app = document.querySelector('.app');
 
+
  let file,
      data;
+
  
+ //ЗАГРУЗКА ФАЙЛА
  inputFile.addEventListener('change', () => {
   file = inputFile.files[0]
 
@@ -19,13 +22,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
  });
 
+//КЛИК ПО КНОПКЕ СГЕНЕРИРОВАТЬ
  createBtn.addEventListener('click', (ev) => {
+  ev.preventDefault();
+ 
   let title = '',
       fields = '',
       btns = '',
-      ref = ''; 
-      
-  ev.preventDefault();
+      ref = '',
+      value = '';
 
   if (typeof(data) === 'object') {
    createBtn.disabled = true;
@@ -34,6 +39,7 @@ window.addEventListener("DOMContentLoaded", () => {
     switch (key) {
     case "name":
       title = data[key];
+      createTitle(title)
       break
     case "fields":
       fields = data[key];
@@ -49,37 +55,188 @@ window.addEventListener("DOMContentLoaded", () => {
 
     }
    })
-  
-   iterateObj(fields);
 
-   function iterateObj(obj) {
-     for(let key in obj) {
-      console.log(key)
-    } 
-   }
+  iterateObj()
 
-  } else {
+ } else {
    app.innerHTML ='вы не загрузили файл';
   }
  
- }, {once: true});
+ });
 
- clearBtn.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  app.innerHTML= ''
-  clearBtn.disabled = true;
- }, {once: true});
+//КЛИК ПО КНОПКЕ ОЧИСТИТЬ
+  clearBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    app.innerHTML= ''
+  });
 
- function upload(file) {
-  const reader = new FileReader()
+//ФУНКЦИИ
 
-  reader.readAsText(file)
+//ПАРСИНГ ФАЙЛА 
+  function upload(file) {
+    const reader = new FileReader()
 
-  reader.onload = () => {
-  data = JSON.parse(reader.result)
+    reader.readAsText(file)
+
+    reader.onload = () => {
+      data = JSON.parse(reader.result)
+    }
 
   }
 
- }
+  //ПЕРЕБОР ЭЛЕМЕНТОВ ФАЙЛА
+  function iterateObj(obj, value) {
+
+    const keys = Object.keys(data)
+    data[keys[1]].forEach((elem, i) => {
+    const fieldsKeys = Object.keys(elem);
+    fieldsKeys.forEach(el => {
+     switch (el) {
+      case "label":
+      createLabel(elem, el)
+       break;
+      case "input":
+      createInput(elem, el)
+       break;
+      default:
+      console.log('ошибка')
+       break;
+     }
+    });
+   });
+
+   //  data[keys[2]].forEach((elem, i) => {
+   //  const refKeys = Object.keys(elem);
+   //  refKeys.forEach(el => {
+   //     switch (el) {
+   //      case "input":
+   //      createInput(elem, el)
+   //       break;
+   //     }
+   //   createLink(elem, el)
+   //  });
+   // });
+
+    data[keys[3]].forEach((elem, i) => {
+    const btnKeys = Object.keys(elem);
+    btnKeys.forEach(el => {
+      console.log(el)
+     switch (el) {
+      case "text":
+      createButtons(elem, el)
+       break;
+      default:
+       console.log('ошибка')
+       break;
+     }
+    });
+   });
+  }
+
+
+    // РАБОТАЕТ !!!!!!
+  //   for(let i = 0; i < obj.length; i++) {
+
+  //     for(let key in obj[i]) {
+
+  //       let value = obj[i][key];
+      
+  //       if(typeof(value) === 'object') {
+  //         iterateObj(value)     
+  //       } else if(typeof(value) === 'array') {
+  //         for(let key in value) {
+  //           console.log(key)  
+  //       }
+  //       } else {
+  //         console.log (value)
+  //     switch(key) {
+  //       case "label": 
+  //       case "input":
+
+  //     }
+  //       }
+  //     }
+  //   }
+  // }
+
+//ИМЯ ФОРМЫ 
+  function createTitle(title) {
+    const h2 = document.createElement('H2');
+    h2.innerHTML = `${title}`
+    app.prepend(h2)
+  }
+
+//ЛЕЙБЛ
+  function createLabel(elem, el) {
+    const label = document.createElement('label');
+    label.innerHTML = `${elem[el]}`
+    label.setAttribute('for', 'input')
+    app.append(label)
+  }
+
+//ИНПУТ
+  function createInput(elem, el) {
+    let input = document.createElement('input')
+    input.setAttribute('id', 'input')
+    app.append(input)
+
+      if(typeof(elem[el]) === 'object') {
+      const value = Object.keys(elem[el]);
+      value.forEach(i => {
+      // console.log(i)
+      // console.log(elem[el][i])
+      switch (i) {
+      case "type":
+       input.setAttribute(`${i}`, `${elem[el][i]}`)
+       break;
+      case "required":
+       input.setAttribute(`${i}`, `${elem[el][i]}`)
+       break;
+      case "checked":
+       input.setAttribute(`${i}`, `${elem[el][i]}`)
+       break;
+      case "placeholder":
+       input.setAttribute(`${i}`, `${elem[el][i]}`)
+       break;
+      // case "filetype":
+
+       break;
+      default:
+      console.log('ошибка')
+       break;
+     } 
+     })
+    } 
+
+  }
+
+// function createLink(elem, el) {
+//   let link = document.createElement('a');
+//   let span = document.createElement('span');
+//   let twh = link.innerHTML;
+
+//         switch (el) {
+//           case "text":
+//           span.innerHTML = `${elem[el]}`
+//          break;
+//         case "ref":
+//         link.setAttribute('href', `${'#' + elem[el]}`)
+//          break;
+//         case "text without ref":
+//         thw = `${elem[el]}`
+//          break;
+//         default:
+//          console.log(elem[el])
+//          break;
+//        }
+//   app.append(link)
+//   link.append(span)
+// }
+
+  function createButtons(elem, el) {
+    let button = document.createElement('button');
+    button.innerHTML = `${elem[el]}`
+    app.append(button)
+  }
 
 })
