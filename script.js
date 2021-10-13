d53f834e6994882c5cd1ce68c6b4fbc1116249bd
Winterh <kotfimn@mail.ru>
2021-10-14 02:27:22 +0700

    Создание форм, добавление чекбоксов по выбору технологий (не добавляются атрибуты к лейблу)

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ window.addEventListener("DOMContentLoaded", () => {
        inputFile = document.querySelector('.file'),
        app = document.querySelector('.app');
 
-
  let file,
-     data;
+     data,
+     label,
+     textarea,
+     input;
 
  
  //ЗАГРУЗКА ФАЙЛА
@@ -20,66 +22,69 @@ window.addEventListener("DOMContentLoaded", () => {
   
   upload(file)
 
+  inputFile.disabled = true;
+
  });
 
-//КЛИК ПО КНОПКЕ СГЕНЕРИРОВАТЬ
- createBtn.addEventListener('click', (ev) => {
-  ev.preventDefault();
- 
-  let title = '',
-      fields = '',
-      btns = '',
-      ref = '',
-      value = '';
+//КЛИК ПО КНОПКЕ СОЗДАТЬ
+  createBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    app.innerHTML= '';
+    let title,
+        fields,
+        btns,
+        ref,
+        value,
+        keys;
 
-  if (typeof(data) === 'object') {
-   createBtn.disabled = true;
+    if (typeof(data) === 'object') {
+      createBtn.disabled = true;
 
-   Object.keys(data).forEach(key => {
-    switch (key) {
-    case "name":
-      title = data[key];
-      createTitle(title)
-      break
-    case "fields":
-      fields = data[key];
-      break
+      Object.keys(data).forEach(key => {
+        switch (key) {
+        case "name":
+         title = data[key];
+         createTitle(title)
+        break
+        case "fields":
+         fields = data[key];
+        break
+        case "references":
+         ref = data[key];
+        break
+        case "buttons":
+         btns = data[key];
+        break
+        }
+      })
 
-    case "references":
-      ref = data[key];
-      break
-
-    case "buttons":
-      btns = data[key];
-      break
-
+      iterateObj()
+    } else {
+      app.innerHTML ='вы не загрузили файл';
     }
-   })
-
-  iterateObj()
-
- } else {
-   app.innerHTML ='вы не загрузили файл';
-  }
  
- });
+  });
 
 //КЛИК ПО КНОПКЕ ОЧИСТИТЬ
   clearBtn.addEventListener('click', (ev) => {
     ev.preventDefault();
-    app.innerHTML= ''
+    app.innerHTML= 'Данные очищены. Выберите новый файл';
+    file = '';
+    createBtn.disabled = false;
+    inputFile.disabled = false;
+
   });
 
 //ФУНКЦИИ
 
 //ПАРСИНГ ФАЙЛА 
   function upload(file) {
-    const reader = new FileReader()
+    const reader = new FileReader();
 
-    reader.readAsText(file)
+    reader.readAsText(file);
 
     reader.onload = () => {
-      data = JSON.parse(reader.result)
+      data = JSON.parse(reader.result);
     }
 
   }
@@ -87,10 +92,10 @@ window.addEventListener("DOMContentLoaded", () => {
   //ПЕРЕБОР ЭЛЕМЕНТОВ ФАЙЛА
   function iterateObj(obj, value) {
 
-    const keys = Object.keys(data)
+    const keys = Object.keys(data);
     data[keys[1]].forEach((elem, i) => {
     const fieldsKeys = Object.keys(elem);
-    fieldsKeys.forEach(el => {
+    fieldsKeys.forEach((el, i) => {
      switch (el) {
       case "label":
       createLabel(elem, el)
@@ -120,7 +125,6 @@ window.addEventListener("DOMContentLoaded", () => {
     data[keys[3]].forEach((elem, i) => {
     const btnKeys = Object.keys(elem);
     btnKeys.forEach(el => {
-      console.log(el)
      switch (el) {
       case "text":
       createButtons(elem, el)
@@ -133,110 +137,127 @@ window.addEventListener("DOMContentLoaded", () => {
    });
   }
 
-
-    // РАБОТАЕТ !!!!!!
-  //   for(let i = 0; i < obj.length; i++) {
-
-  //     for(let key in obj[i]) {
-
-  //       let value = obj[i][key];
-      
-  //       if(typeof(value) === 'object') {
-  //         iterateObj(value)     
-  //       } else if(typeof(value) === 'array') {
-  //         for(let key in value) {
-  //           console.log(key)  
-  //       }
-  //       } else {
-  //         console.log (value)
-  //     switch(key) {
-  //       case "label": 
-  //       case "input":
-
-  //     }
-  //       }
-  //     }
-  //   }
-  // }
-
-//ИМЯ ФОРМЫ 
+//СОЗДАЕМ ИМЯ ФОРМЫ 
   function createTitle(title) {
     const h2 = document.createElement('H2');
-    h2.innerHTML = `${title}`
+    h2.innerHTML = `${title}`;
     app.prepend(h2)
   }
 
-//ЛЕЙБЛ
+//CОЗДАЕМ ЛЕЙБЛ
   function createLabel(elem, el) {
-    const label = document.createElement('label');
-    label.innerHTML = `${elem[el]}`
-    label.setAttribute('for', 'input')
+    label = document.createElement('label');
+    label.innerHTML = `${elem[el]}`;
     app.append(label)
   }
 
-//ИНПУТ
+//СОЗДАЕМ ИНПУТ
   function createInput(elem, el) {
-    let input = document.createElement('input')
-    input.setAttribute('id', 'input')
-    app.append(input)
+    input = document.createElement('input');
+    app.append(input);
 
-      if(typeof(elem[el]) === 'object') {
-      const value = Object.keys(elem[el]);
-      value.forEach(i => {
-      // console.log(i)
-      // console.log(elem[el][i])
-      switch (i) {
-      case "type":
-       input.setAttribute(`${i}`, `${elem[el][i]}`)
-       break;
-      case "required":
-       input.setAttribute(`${i}`, `${elem[el][i]}`)
-       break;
-      case "checked":
-       input.setAttribute(`${i}`, `${elem[el][i]}`)
-       break;
-      case "placeholder":
-       input.setAttribute(`${i}`, `${elem[el][i]}`)
-       break;
-      // case "filetype":
+    if(typeof(elem[el]) === 'object') {
+      let key = Object.keys(elem[el]);
 
-       break;
-      default:
-      console.log('ошибка')
-       break;
-     } 
-     })
+      key.forEach(i => {
+        switch (i) {
+        case "type":
+         let id = createId()
+         if(elem[el][i] === 'textarea') {
+            textarea = document.createElement('textarea');
+            app.removeChild(input)
+            let value = Object.values(elem[el])
+            textarea.setAttribute(`${key[1]}`, `${value[1]}`)
+            app.append(textarea)
+         } else {
+           input.setAttribute(`${i}`, `${elem[el][i]}`) 
+           input.setAttribute('id', `${id}`)
+           if(label === true) {
+             label.setAttribute('for', `${id}`)      
+           } 
+        }
+        break;
+        case "required":
+         input.setAttribute(`${i}`, `${elem[el][i]}`)     
+        break;
+        case "checked":
+         input.setAttribute(`${i}`, `${elem[el][i]}`)
+        break;
+        case "placeholder":
+         input.setAttribute(`${i}`, `${elem[el][i]}`)
+        break;
+        case "filetype":
+         createFileType(i, elem[el][i])
+        break;
+        case "technologies":
+         createTechnology(i, elem[el][i])
+        break;
+        case "multiple":
+         input.setAttribute(`${i}`, `${elem[el][i]}`)
+        break;
+        default:
+         console.log('ошибка')
+        break;
+       } 
+      });
     } 
 
   }
 
-// function createLink(elem, el) {
-//   let link = document.createElement('a');
-//   let span = document.createElement('span');
-//   let twh = link.innerHTML;
-
-//         switch (el) {
-//           case "text":
-//           span.innerHTML = `${elem[el]}`
-//          break;
-//         case "ref":
-//         link.setAttribute('href', `${'#' + elem[el]}`)
-//          break;
-//         case "text without ref":
-//         thw = `${elem[el]}`
-//          break;
-//         default:
-//          console.log(elem[el])
-//          break;
-//        }
-//   app.append(link)
-//   link.append(span)
-// }
-
+//СОЗДАЕМ КНОПКУ
   function createButtons(elem, el) {
     let button = document.createElement('button');
-    button.innerHTML = `${elem[el]}`
+    button.innerHTML = `${elem[el]}`;
     app.append(button)
   }
 
-})
+
+//СОЗДАЕМ АТРИБУТ TYPEFILE 
+  function createFileType(i, type) {
+
+   let fileType = type.join('/');
+   input.setAttribute('accept', `${fileType}`);
+  }
+
+
+//СОЗДАЕМ ТЕХНОЛОГИИ
+  function createTechnology(i, technologies) {
+
+    let tech = technologies;
+    let formTech = document.createElement('form');
+    formTech.className = 'formTech';
+    app.removeChild(input);
+    app.append(formTech);
+    tech.forEach(i => {
+      let id = createId()
+      let checkboxTech = document.createElement('div');
+      checkboxTech.className = 'technology';
+      let inputTech = document.createElement('input');
+      let labelTech = document.createElement('label');
+      inputTech.setAttribute('type', 'checkbox');
+      inputTech.setAttribute('name', 'technologies');
+      inputTech.setAttribute('id', `${id}`);
+      labelTech.setAttribute('for', `${id}`);
+      labelTech.innerHTML = `${i}`;
+      formTech.append(checkboxTech);
+      checkboxTech.append(inputTech);
+      checkboxTech.append(labelTech);
+    })
+  }
+
+//СОЗДАЕМ ID ДЛЯ ИНПУТОВ И ЛЕЙБЛОВ
+  function createId() {
+    let random_start = 1;
+    let random_end = 1000;
+    allСycles = 20;
+    let array= []
+    for(i=random_start;i<=random_end;i++){
+       array.push(i)
+    }
+    for(countCycles=1;countCycles<=allСycles;countCycles++){
+        return(array.splice(Math.random()*array.length,1)[0])
+    }
+
+  }
+
+});
